29fc645f7fb8112980c8561a4fb83c586c3746e1
Aleksey Chistov <aleksey.chistov@gmail.com>
2018-07-05 12:47:53 +0300

    Created public interface class. It grants ability for other linked mods to check if item is "AutoTrashed".

---
+++ AutoTrashInterface.cs
@@ -0,0 +1,28 @@
+﻿
+using Terraria;
+
+
+namespace AutoTrash
+{
+    static public class AutoTrashInterface
+    {
+        static public bool isItemAutoTrashedByPlayer(Player player, int item_type)
+        {
+            // Returns False if AutoTrash is disabled
+            // Returns False if item is not 'AutoTrashed'
+            // Returns True if item is 'AutoTrashed'
+            var mod_player = player.GetModPlayer<AutoTrashPlayer>();
+
+            if (!mod_player.AutoTrashEnabled)
+                return false;
+
+            foreach (var item in mod_player.AutoTrashItems)
+            {
+                if (item_type == item.type)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
